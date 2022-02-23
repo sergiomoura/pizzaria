ab67bf684aaf5c1b8942b4c07a962232e8d30ca8
Sérgio Moura <smouracalmon@gmail.com>
2022-02-23 16:29:09 -0300

    Criando uma página para mostrar detalhes de uma pizza

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -3,5 +3,11 @@ let pizzas = require('../database/Pizzas.json');
 module.exports = {
     listar: (req,res) => {
         res.render('pizzas.ejs', {pizzas});
+    },
+    mostrar: (req,res) => {
+        let id = req.params.id;
+        let pizza = pizzas.find(p => p.id == id);
+        res.render('pizza.ejs',{pizza});
     }
+
 }
--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -3,5 +3,6 @@ const PizzasController = require('../controllers/PizzasController');
 const router = express.Router();
 
 router.get('/', PizzasController.listar);
+router.get('/pizzas/:id', PizzasController.mostrar)
 
 module.exports = router;
